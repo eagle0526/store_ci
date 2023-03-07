c37bd053e147b4e3fe5c75f89198b0b5e06a92f3
Yee <a034506618@gmail.com>
2023-03-07 13:25:14 +0800

    simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,6 +47,8 @@ Capybara.javascript_driver = :headless_chrome
 
 
 
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
